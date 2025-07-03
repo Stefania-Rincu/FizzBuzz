b20f6b4ba8aba142290b91cbe7a988d4d03dbce7
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-03 14:44:01 +0300

    add auxiliary functions

--- index.ts
+++ index.ts
@@ -1,52 +1,55 @@
+// Function that constructs the output based on a list of messages and a boolean flag
+function constructOutput(messages: string[], revert: boolean): string {
+    return revert ? messages.reverse().join('') : messages.join('');
+}
+
+// Function that constructs the list of messages and returns the actual output
+function messageToPrint(num: number): string {
+    // Keeps the list of messages
+    const messageList: string[] = [];
+    // Flag used to know if the output needs to be reversed
+    const revert: boolean = (num % 17 === 0);
+
+    // If a number is a multiple of 11, then just 13 or 11 influences the output
+    if (num % 11 === 0) {
+        messageList.push('Bong');
+
+        // If a number is multiple of 11 and 13, add 'Feez' before 'Bong'
+        if (num % 13 === 0)
+            messageList.splice(0, 0, 'Feez');
+
+        // Call the function that constructs the output
+        return constructOutput(messageList, revert);
+    }
+
+    // If a number is multiple of 3, add 'Fizz'
+    if (num % 3 === 0)
+        messageList.push('Fizz');
+
+    // If a number is multiple of 13, add 'Feez'
+    if (num % 13 === 0)
+        messageList.push('Feez');
+
+    // If a number is multiple of 5, add 'Buzz'
+    if (num % 5 === 0)
+        messageList.push('Buzz');
+
+    // If a number is multiple of 7, add 'Bang'
+    if (num % 7 === 0)
+        messageList.push('Bang');
+
+    if (messageList.length === 0)
+        return num.toString();
+
+    return constructOutput(messageList, revert);
+}
+
 // This is our main function
 function fizzbuzz(): void {
-    // Iterate through numbers in interval [1, 10]
-    for (let i:number = 1; i <= 143; i++) {
-        // Keeps a list of the words that need to be printed
-        let printLog:string[] = [];
-        // Keeps the actual output
-        let toPrint:string = '';
-
-        // If a number is multiple of 3, add 'Fizz'
-        if (i % 3 === 0)
-            printLog.push('Fizz');
-
-        // If a number is multiple of 13, add 'Feez'
-        if (i % 13 === 0)
-            printLog.push('Feez');
-
-        // If a number is multiple of 5, add 'Buzz'
-        if (i % 5 === 0)
-            printLog.push('Buzz');
-
-        // If a number is multiple of 7, add 'Bang'
-        if (i % 7 === 0)
-            printLog.push('Bang');
-
-        // If a number is multiple of 11, add 'Bong'
-        if (i % 11 === 0) {
-            // If just 'Feez' or nothing was added, add 'Bong'
-            if (printLog.length === 0 || (printLog.length === 1 && printLog[0] === 'Feez'))
-                printLog.push('Bong');
-            // Else remove everything and add 'Bong' and 'Feez' if needed
-            else {
-                printLog = ['Bong'];
-                if (i % 13 === 0)
-                    printLog.splice(0, 1, 'Feez');
-            }
-        }
-
-        // Check if nothing was added to the printLog list (not a multiple of any "special" number)
-        if (printLog.length === 0)
-            toPrint = i.toString();
-        else {
-            // If a number is multiple of 17, reverse the order of the output
-            if (i % 17 === 0)
-                toPrint = printLog.reverse().join('');
-            else
-                toPrint = printLog.join('');
-        }
-        console.log(toPrint);
+    // Iterate through numbers in interval [1, 255]
+    for (let i:number = 1; i <= 255; i++) {
+        const output: string = messageToPrint(i);
+        console.log(output);
     }
 }
 
