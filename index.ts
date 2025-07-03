bd4b14012b87fee931048c9522e68d86d1273f7d
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-03 11:39:38 +0300

    add solution for part 1 of the exercise

--- index.ts
+++ index.ts
@@ -1,7 +1,20 @@
 // This is our main function
 function fizzbuzz(): void {
-    console.log("Hello, World!");
-    // Put your code here...
+    // Iterate through numbers in interval [1, 10]
+    for (let i:number = 1; i <= 100; i++) {
+        // If a number is multiple of both 3 and 5, print 'FizzBuzz'
+        if (i % 15 === 0)
+            console.log("FizzBuzz");
+        // If a number is multiple of 3, print 'Fizz'
+        else if (i % 3 === 0)
+            console.log("Fizz");
+        // If a number is multiple of 5, print 'Buzz'
+        else if (i % 5 === 0)
+            console.log("Buzz");
+        // Else, print the number
+        else
+            console.log(i);
+    }
 }
 
 // Now, we run the main function:
