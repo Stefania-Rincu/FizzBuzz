b87098863b7759f7a2e8605f122899df83c523c2
Stefania Rincu <Stefania.Rincu@softwire.com>
2025-07-03 16:53:16 +0300

    add logic to let user introduce prompts

--- index.ts
+++ index.ts
@@ -1,21 +1,24 @@
+//import prompt from "./user-prompt"
+import {createInterface} from "readline";
+
 // Function that constructs the output based on a list of messages and a boolean flag
 function constructOutput(messages: string[], revert: boolean): string {
     return revert ? messages.reverse().join('') : messages.join('');
 }
 
 // Function that constructs the list of messages and returns the actual output
-function messageToPrint(num: number): string {
+function messageToPrint(num: number, rulesToUse: number[]): string {
     // Keeps the list of messages
     const messageList: string[] = [];
     // Flag used to know if the output needs to be reversed
-    const revert: boolean = (num % 17 === 0);
+    const revert: boolean = (17 in rulesToUse && num % 17 === 0);
 
     // If a number is a multiple of 11, then just 13 or 11 influences the output
-    if (num % 11 === 0) {
+    if (11 in rulesToUse && num % 11 === 0) {
         messageList.push('Bong');
 
         // If a number is multiple of 11 and 13, add 'Feez' before 'Bong'
-        if (num % 13 === 0)
+        if (13 in rulesToUse && num % 13 === 0)
             messageList.splice(0, 0, 'Feez');
 
         // Call the function that constructs the output
@@ -23,19 +26,19 @@ function messageToPrint(num: number): string {
     }
 
     // If a number is multiple of 3, add 'Fizz'
-    if (num % 3 === 0)
+    if (3 in rulesToUse && num % 3 === 0)
         messageList.push('Fizz');
 
     // If a number is multiple of 13, add 'Feez'
-    if (num % 13 === 0)
+    if (13 in rulesToUse && num % 13 === 0)
         messageList.push('Feez');
 
     // If a number is multiple of 5, add 'Buzz'
-    if (num % 5 === 0)
+    if (5 in rulesToUse && num % 5 === 0)
         messageList.push('Buzz');
 
     // If a number is multiple of 7, add 'Bang'
-    if (num % 7 === 0)
+    if (7 in rulesToUse && num % 7 === 0)
         messageList.push('Bang');
 
     if (messageList.length === 0)
@@ -45,13 +48,30 @@ function messageToPrint(num: number): string {
 }
 
 // This is our main function
-function fizzbuzz(): void {
-    // Iterate through numbers in interval [1, 255]
-    for (let i:number = 1; i <= 255; i++) {
-        const output: string = messageToPrint(i);
+function fizzbuzz(maxValue: number, rulesToUse: number[]): void {
+    // Iterate through numbers in interval [1, maxValue]
+    for (let i:number = 1; i <= maxValue; i++) {
+        const output: string = messageToPrint(i, rulesToUse);
         console.log(output);
     }
 }
 
-// Now, we run the main function:
-fizzbuzz();
+// let maxNumber = await prompt("Enter number: ");
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter maximum number: ",
+    (answer: string) => {
+    try {
+        const maxNumber = parseInt(answer, 10);
+        rl.question("Introduce rules to be used using space between each number (options: 3 5 7 11 13 17): ", (rules: string) => {
+            const rulesList: number[] = rules.split(' ').map(rule => parseInt(rule, 10));
+            fizzbuzz(maxNumber, rulesList);
+            rl.close()});
+    } catch (e) {
+        console.error("Introduce a number!");
+        rl.close();
+    }
+});
